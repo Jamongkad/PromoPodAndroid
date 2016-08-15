83f692472e305f4d899886da2801a0156f8d9d47
jamongkad <wrm932@gmail.com>
2016-08-15 15:53:02 +0800

    Almost there

--- www/js/DashCtrl.js
+++ www/js/DashCtrl.js
@@ -1,4 +1,24 @@
 angular.module('starter.controllers')
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $state, AirFare) {
 
+    $scope.flights = [];
+    AirFare.getGroupedFlights().then(function successCallback(response) {
+        $scope.flights = response.data;
+    }, function errorCallback(response) {
+        console.log("Failed: " + response.statusText);
+    })
+
+    $scope.openFlight = function(flight) {
+        $state.go('flights', {'flightId': flight.flights});
+    }
+
+    $scope.thousands = function(price) {
+        return numberWithCommas(price);
+    }
+
+    function numberWithCommas(x) {
+        var parts = x.toString().split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return parts.join(".");
+    }
 });
