98230522d5c5bb7db28866a2a86967e96a571598
jamongkad <wrm932@gmail.com>
2016-08-19 15:21:08 +0800

    100%

--- www/js/DetailCtrl.js
+++ www/js/DetailCtrl.js
@@ -5,6 +5,22 @@ angular.module('starter.controllers')
 
     $scope.goToWebsite = function() {
         console.log($scope.flightObj);
+
+        if($scope.flightObj.provider == "airasia") {
+            window.open('http://www.airasia.com', '_system', 'location=yes'); 
+        }
+
+        if($scope.flightObj.provider == "cebupac") {
+            window.open('https://www.cebupacificair.com', '_system', 'location=yes'); 
+        }
+
+        if($scope.flightObj.provider == "jetstar") {
+            window.open('https://www.jetstar.com', '_system', 'location=yes'); 
+        }
+
+        if($scope.flightObj.provider == "tigerair") {
+            window.open('https://www.tigerair.com', '_system', 'location=yes'); 
+        }
     }
 
     $scope.numberWithCommas = function(price) {
